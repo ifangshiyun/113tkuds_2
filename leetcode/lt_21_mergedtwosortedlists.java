1e41f3739a061c0f9ae35ec4a73fc89ad3d5ac75
ifangshiyun <114101384+ifangshiyun@users.noreply.github.com>
2025-09-05 00:47:37 +0700

    Create lt_21_mergedtwosortedlists.java

---
+++ leetcode/lt_21_mergedtwosortedlists.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode tail = dummy;
+
+        ListNode a = list1, b = list2;
+        while (a != null && b != null) {
+            if (a.val <= b.val) {
+                tail.next = a;
+                a = a.next;
+            } else {
+                tail.next = b;
+                b = b.next;
+            }
+            tail = tail.next;
+        }
+
+        // Attach the remaining nodes (only one of a or b is non-null)
+        tail.next = (a != null) ? a : b;
+
+        return dummy.next;
+    }
+}
