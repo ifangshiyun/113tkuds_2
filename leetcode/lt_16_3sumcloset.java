8e008a1be5b240e036294d5678e32f4735eaa569
ifangshiyun <114101384+ifangshiyun@users.noreply.github.com>
2025-09-05 00:41:00 +0700

    Create lt_16_3sumcloset.java

---
+++ leetcode/lt_16_3sumcloset.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int best = nums[0] + nums[1] + nums[2];
+
+        for (int i = 0; i < n - 2; i++) {
+            int left = i + 1, right = n - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (Math.abs(sum - target) < Math.abs(best - target)) {
+                    best = sum;
+                }
+
+                if (sum == target) return sum;
+                if (sum < target) left++;
+                else right--;
+            }
+        }
+        return best;
+    }
+}
